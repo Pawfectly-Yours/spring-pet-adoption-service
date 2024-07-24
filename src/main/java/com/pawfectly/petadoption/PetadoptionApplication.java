4a469ba6a0b6ded4dcec4d10ca6237783818c838
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2024-07-24 10:49:50 +0800

    first commit

---
+++ src/main/java/com/pawfectly/petadoption/PetadoptionApplication.java
@@ -0,0 +1,13 @@
+package com.pawfectly.petadoption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetadoptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetadoptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pawfectly/petadoption/PetadoptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawfectly.petadoption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetadoptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
